fd7c5935538b6c23dbabc8fb3c95c9f7cd5115bc
RaphaelMacedoB <faeldojo@gmail.com>
2023-12-26 17:37:51 -0300

    ProjetoVue

---
+++ Documents/VueJSRaphael/2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Meu nome é: {{ nome }}</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "LifeCycle",
+    data() {
+        return {
+            nome: "Ainda não sei"
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.nome = "Raphael"
+    }, 1000);
+    },
+    mounted() {
+        setTimeout(() => {
+            
+            this.nome = "Pedro"
+    }, 2000);
+    },
+}
+</script>
